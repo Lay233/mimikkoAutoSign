86d1992ef94d24f3ad95e1cd79368ad4947c6ad0
Lay233 <47231064+Lay233@users.noreply.github.com>
2021-04-10 14:12:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -25,8 +25,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 $userinfo = getopt('u:p:');
 
 $config = [
-    "782004797@qq.com" => $userinfo['u'],
-    "123456789" => $userinfo['p']
+    "user" => 782004797@qq.com['u'],
+    "password" => 123456789['p']
 ];
 
 $mimikko = new Mimikko($config["user"], $config['password']);
